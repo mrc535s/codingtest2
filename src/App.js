69442cc1f4fecc3aeb5063b3eaba168f855b46bb
Max Chen <chenr.maxim@gmail.com>
2017-07-17 14:07:22 -0700

    tutorial react stage:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+        projects: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      projects: [
+      {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Application',
+          category: 'Web Development'
+        }
+      ],
+      movieTitles: [
+        {
+          title: 'Lord of the Rings'
+        },
+        {
+          title: 'Harry Potter'
+        },
+        {
+          title: 'Pulp Fiction'
+        },
+        {
+          title: 'Avengers'
+        },
+        {
+          title: 'Indiana Jones'
+        },
+        {
+          title: 'Finding Nemo'
+        }
+      ]
+    });
+  }
+
+  handleAddProject(project) {
+    console.log(project);
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <h1>Project:</h1>
+          <AddProject addProject={this.handleAddProject.bind(this)}/>
+          <Projects projects={this.state.projects}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h1> Movie Titles: </h1>
+        </div>
       </div>
     );
   }
